b90da172217b714b9cb253e225685810df688fe5
Riku <71032160+rikurikurikuriku@users.noreply.github.com>
2021-10-21 21:03:43 +0300

    Delete Project_v2 directory

--- Project_v2/src/main/java/InvoiceProject/Project/domain/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package InvoiceProject.Project.domain;
-
-import org.springframework.data.repository.CrudRepository; 
-
-public interface UserRepository extends CrudRepository<User, Long> {
-	User findByUsername(String username);
-}
-
-
